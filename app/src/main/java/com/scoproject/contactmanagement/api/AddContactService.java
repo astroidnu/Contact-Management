0cf0399f8e471ea3c00dc0c9f74e77786db451bb
Ibnu Muzzakkir <personalict@gmail.com>
2017-03-03 14:10:52 +0700

    add reactive x complete to handle response from observer retrofit  in add contact service page

--- app/src/main/java/com/scoproject/contactmanagement/api/AddContactService.java
+++ app/src/main/java/com/scoproject/contactmanagement/api/AddContactService.java
@@ -1,8 +1,16 @@
 package com.scoproject.contactmanagement.api;
 
+
+import com.scoproject.contactmanagement.data.Contact;
 import com.scoproject.contactmanagement.network.NetworkService;
 
 import java.util.HashMap;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by ibnumuzzakkir on 02/03/2017.
@@ -12,6 +20,7 @@ import java.util.HashMap;
 
 public class AddContactService {
     private NetworkService mNetworkService = null;
+    private HashMap<String, String> data;
     public void init(HashMap<String, String> param){
 
     }
@@ -19,5 +28,38 @@ public class AddContactService {
     public AddContactService(NetworkService networkService){
         mNetworkService = networkService;
 
+    }
+
+    public void addContact(final GetResponseCallback callback){
+        mNetworkService.addContact(data)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .onErrorResumeNext(throwable -> {
+                    return Observable.error(throwable);
+                })
+                .subscribe(new Observer<Contact>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(Contact value) {
+                        callback.onSuccess(value);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        callback.onError(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+    }
+
+    public interface GetResponseCallback{
+        void onSuccess(Contact dataList);
+        void onError(Throwable e);
     }
 }
